8c40afd1f2354667a5925c2f8727bfd04b799622
antoino filoho <antoniofilhodaconceicaorozafil@gmail.com>
2024-02-16 18:11:31 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+$('.radio1').click(function() {
+    $('#carouselExampleIndicators').carousel(0);
+});
+$('.radio2').click(function() {
+    $('#carouselExampleIndicators').carousel(1);
+});
+$('.radio3').click(function() {
+    $('#carouselExampleIndicators').carousel(2);
+});
+$('.radio4').click(function() {
+    $('#carouselExampleIndicators').carousel(0);
+});
