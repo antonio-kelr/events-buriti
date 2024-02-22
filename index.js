cf970e144e34dd1214d029b82b8cd2955df7fd4d
antoino filoho <antoniofilhodaconceicaorozafil@gmail.com>
2024-02-22 16:26:26 -0300

    alteracao em navBar

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-$('.radio1').click(function() {
+const butao = document.querySelector('.butao');
+const contador = document.querySelector('.contador');
+let contadorNumero = 0; 
+
+butao.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    contadorNumero++;
+    contador.textContent = contadorNumero;
+});
+
+
+
+
+
+
+
+$('.radio1').click(function () {
     $('#carouselExampleIndicators').carousel(0);
 });
-$('.radio2').click(function() {
+$('.radio2').click(function () {
     $('#carouselExampleIndicators').carousel(1);
 });
-$('.radio3').click(function() {
+$('.radio3').click(function () {
     $('#carouselExampleIndicators').carousel(2);
 });
-$('.radio4').click(function() {
+$('.radio4').click(function () {
     $('#carouselExampleIndicators').carousel(0);
 });
+
